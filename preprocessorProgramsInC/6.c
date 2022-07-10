3aeaacc8a271be4c0f6fe4ae03955f99cdd9c4a0
Priyanuj Bora <www.priyanuj.bora13@gmail.com>
2022-07-10 20:31:43 +0530

    Generic functions

---
+++ preprocessorProgramsInC/6.c
@@ -0,0 +1,37 @@
+// Generic functions
+
+// SIMILAR TO IMPLEMENTING TEMPLATES IN C++
+
+#include<stdio.h>
+#define MAX(fname, dtype) \
+	dtype fname(dtype x, dtype y) \
+{\
+	return x>y?x:y; \
+}
+
+
+MAX(maxint, int);
+MAX(maxfloat, float);
+
+int main(void)
+{
+	int p, q;
+	float a, b;
+
+	printf("enter value of p and q: ");
+	scanf("%d-%d", &p, &q);
+
+	printf("enter value of a and b: ");
+	scanf("%f-%f", &a, &b);
+
+	int result1;
+	float result2;
+
+	result1=maxint(p, q);
+	result2=maxfloat(a, b);
+
+	printf("\n\nThe maximum of %d and %d is %d\n\n", p, q, result1);
+	printf("\n\nThe maximum of %.2f and %.2f is %.2f\n\n", a, b, result2);
+
+	return 0;
+}
